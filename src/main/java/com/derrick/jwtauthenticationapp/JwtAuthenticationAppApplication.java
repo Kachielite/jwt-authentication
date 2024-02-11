0f3c870fd8cac80741514fcb90ca2b0f32b8172a
Kachielite <40024356+Kachielite@users.noreply.github.com>
2024-02-11 22:39:31 +0300

    set up datasource and add configs to app.yml file

---
+++ src/main/java/com/derrick/jwtauthenticationapp/JwtAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.derrick.jwtauthenticationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationAppApplication.class, args);
+    }
+
+}
