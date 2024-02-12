1ec417d6e0e2ff36d41fb05e264f4d07e369b2fe
Kachielite <40024356+Kachielite@users.noreply.github.com>
2024-02-12 16:25:50 +0300

    set up datasource connection, user repository and added User Entity

---
+++ src/main/java/com/derrick/jwtauthenticationapp/entity/Role.java
@@ -0,0 +1,6 @@
+package com.derrick.jwtauthenticationapp.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/derrick/jwtauthenticationapp/utils/AuthUtils.java
@@ -0,0 +1,8 @@
+package com.derrick.jwtauthenticationapp.utils;
+
+
+
+public class AuthUtils {
+    public static final  String USER_NOT_FOUND_CODE = "001";
+    public static final String USER_NOT_FOUND_MESSAGE = "User not found";
+}
